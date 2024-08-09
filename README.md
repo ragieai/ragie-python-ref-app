8af3c2b2cf3da7e32cdef47127fb9f82d9c2a218
Matt Kauffman <matt@ragie.ai>
2024-08-09 14:52:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Intent
+
+This is an example project to demonstrate some of the features of ragie.ai's [python SDK](https://pypi.org/project/ragie/).
+
+## Getting Started
+
+First provide your ragie api key by creating .env file with the following env variable:
+`RAGIE_API_KEY=<YOUR_KEY>`
+
+This project uses [poetry](https://python-poetry.org/) as a pacakge manager. Install it if you haven't already.
+
+```bash
+poetry install
+poetry run test
+```
---
+++ ragie_python_ref_app/main.py
@@ -0,0 +1,47 @@
+import os
+import time
+
+from dotenv import load_dotenv
+from ragie import Ragie
+
+load_dotenv()
+
+ragie = Ragie(
+    auth=os.getenv('RAGIE_API_KEY'),
+)
+
+def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'examples', 'sample_podcast.txt')
+    create_res = ragie.documents.create(request={
+        "file": {
+            "file_name": "sample_podcast.txt",
+            "content": open(file_path, "rb"),
+        },
+        "metadata": {
+            "scope": "tutorial"
+        }
+    })
+
+    print(create_res)
+
+    while True:
+        res = ragie.documents.get(document_id=create_res.id)
+        print(res.status)
+        if res.status == "ready":
+            break
+        
+        time.sleep(2)
+
+    retrieval_res = ragie.retrievals.retrieve(request={
+        "query": "What does Chamath think about Davos?",
+        "filter_": {
+            "scope": "tutorial"
+        },
+        "rerank": True
+    })
+
+    print(retrieval_res)
+
+if __name__ == "__main__":
+    main()
